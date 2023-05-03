59a542acedcb24d4c460639cc40440970473a3a9
Prashant <prashantkarki630@gmail.com>
2023-05-03 21:17:19 +0545

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PersonIcon from "@material-ui/icons/Person";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import AddIcon from "@material-ui/icons/Add";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
+      <SidebarOption active text="Home" Icon={HomeIcon} />
+      <SidebarOption text="Explore" Icon={SearchIcon} />
+      <SidebarOption text="Notifications" Icon={NotificationsIcon} />
+      <SidebarOption text="Message" Icon={MailOutlineIcon} />
+      <SidebarOption text="Bookmarks" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="Profile" Icon={PersonIcon} />
+      <SidebarOption text="More" Icon={MoreHorizIcon} />
+      <SidebarOption text="Tweet" Icon={AddIcon} />
+    </div>
+  );
+}
+
+export default Sidebar;
