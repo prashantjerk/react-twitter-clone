52273238a8ae208ab71632646937bbe881b025a3
Prashant <prashantkarki630@gmail.com>
2023-05-11 20:34:30 +0545

    tweet box initial look finished

--- src/Components/TweetBox.js
+++ src/Components/TweetBox.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./TweetBox.css";
 import TweetBoxIcon from "./TweetBoxIcon";
 import { Avatar } from "@mui/material";
-import PhotoIcon from "@material-ui/icons/Photo";
+import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
+import GifBoxOutlinedIcon from "@mui/icons-material/GifBoxOutlined";
+import PollOutlinedIcon from "@mui/icons-material/PollOutlined";
+import SentimentSatisfiedOutlinedIcon from "@mui/icons-material/SentimentSatisfiedOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import FmdGoodOutlinedIcon from "@mui/icons-material/FmdGoodOutlined";
+import Button from "@material-ui/core/Button";
 
 function TweetBox() {
   return (
@@ -14,14 +20,27 @@ function TweetBox() {
           sx={{ width: 50, height: 50 }}
           className="avatar"
         />
-        <input
+        <textarea
           className="input_tweet"
-          type="text"
           placeholder="What's happening?"
-        />
+        ></textarea>
       </div>
+
       {/* icons */}
-      <TweetBoxIcon IconComponent={PhotoIcon} />
+      <div className="tweetbox_foot">
+        <div className="tweetbox_foot_icons">
+          <TweetBoxIcon IconComponent={AddPhotoAlternateOutlinedIcon} />
+          <TweetBoxIcon IconComponent={GifBoxOutlinedIcon} />
+          <TweetBoxIcon IconComponent={PollOutlinedIcon} />
+          <TweetBoxIcon IconComponent={SentimentSatisfiedOutlinedIcon} />
+          <TweetBoxIcon IconComponent={CalendarMonthOutlinedIcon} />
+          <TweetBoxIcon IconComponent={FmdGoodOutlinedIcon} />
+        </div>
+
+        <Button variant="contained" className="tweetBox__tweet">
+          Tweet
+        </Button>
+      </div>
     </div>
   );
 }
